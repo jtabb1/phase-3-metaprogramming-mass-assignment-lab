954f6a7373f2e0f38fa7b3ddb4a1ae751aac2b2c
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-17 14:28:56 -0400

    additional logic in official solution also passes tests and gives an extra feature;

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,20 @@
 class Person
-  def initialize(attributes)
+  # original solution below passes tests
+  def initialize_o(attributes)
     attributes.each do |key, value|
       self.class.attr_accessor(key)
       self.send("#{key}=", value)
+    end
+  end
+
+  # additional logic in official solution gives an
+  # extra feature and also passes the tests:
+  def initialize(attributes = nil)
+    if attributes
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
     end
   end
 end
